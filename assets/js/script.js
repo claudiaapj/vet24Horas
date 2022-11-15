65b13279c10cc8cc305027e3462564c722594567
Claudia Aguilar Da Paixao De Jesus <claudia.aguilar@cedro.ind.br>
2022-11-15 15:17:01 -0300

    Atualizando projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
+let count = 1;
 
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    mudaimg();
+}, 2000)
   
+function mudaimg(){
+    count++;
+    if(count >3){
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
