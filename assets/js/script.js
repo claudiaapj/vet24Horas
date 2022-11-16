675bdc58edf1663b764dd7d6617268b1be546a28
Claudia Aguilar Da Paixao De Jesus <claudia.aguilar@cedro.ind.br>
2022-11-15 22:44:36 -0300

    validando formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let count = 1;
 
 document.getElementById("radio1").checked = true;
 
+const erro = document.getElementById("erro");
+
 setInterval(function(){
     mudaimg();
 }, 2000)
@@ -13,3 +15,46 @@ function mudaimg(){
     }
     document.getElementById("radio"+count).checked = true;
 }
+
+
+
+function validaForm(frm) {
+    /*
+    o parâmetro frm desta função significa: this.form,
+    pois a chamada da função - validaForm(this) foi
+    definida na tag form.
+    */
+        //Verifica se o campo nome foi preenchido e
+        //contém no mínimo três caracteres.
+        if(frm.nome.value == "" || frm.nome.value == null || frm.nome.value.lenght < 3) {
+            //É mostrado um alerta, caso o campo esteja vazio.
+           // alert("Por favor, indique o seu nome.");
+           if(erro.classList = "none"){
+           erro.classList.remove("none")
+            erro.innerHTML = "Preecha o nome!"
+           }else{
+            erro.classList.add("none")
+           }
+
+
+            //Foi definido um focus no campo.
+            frm.nome.focus();
+            //o form não é enviado.
+            return false;
+        }
+        //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
+        if(frm.email.value.indexOf("@") == -1 ||
+          frm.email.valueOf.indexOf(".") == -1 ||
+          frm.email.value == "" ||
+          frm.email.value == null) {
+            if(erro.classList = "none"){
+                erro.classList.remove("none")
+                 erro.innerHTML = "Preecha o e-mail corretamente!"
+                }else{
+                 erro.classList.add("none")
+                }
+            frm.email.focus();
+            return false;
+        }
+       
+    }
